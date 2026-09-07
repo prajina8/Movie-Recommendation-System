66d25453497911f9d8452287bc58897fafa1fa5a
prajina-Adhikari <aprajina45@gmail.com>
2026-09-07 21:05:25 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <div className="app-shell">
+      <Navbar />
+      <Routes/>
+        
+      <Footer />
+    </div>
+  );
+}
