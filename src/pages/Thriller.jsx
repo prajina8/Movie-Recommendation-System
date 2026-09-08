60c253480733b91d723e3c8a77f9a76f7bcb52a3
prajina-Adhikari <aprajina45@gmail.com>
2026-09-08 22:17:26 +0545

    added new page

---
+++ src/pages/Thriller.jsx
@@ -0,0 +1,11 @@
+import GenrePage from "../components/GenrePage.jsx";
+
+export default function Thriller() {
+  return (
+    <GenrePage
+      genreId={53}
+      title="Thriller"
+      description="Tense, high-stakes picks to keep you on edge."
+    />
+  );
+}
