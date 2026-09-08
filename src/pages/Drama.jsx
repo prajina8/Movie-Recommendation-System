ce3a7169e2c7f2bb7c69da36100cb766302afa6d
prajina-Adhikari <aprajina45@gmail.com>
2026-09-08 22:15:35 +0545

    added new page

---
+++ src/pages/Drama.jsx
@@ -0,0 +1,11 @@
+import GenrePage from "../components/GenrePage.jsx";
+
+export default function Drama() {
+  return (
+    <GenrePage
+      genreId={18}
+      title="Drama"
+      description="Character-driven stories, fetched fresh from TMDB."
+    />
+  );
+}
