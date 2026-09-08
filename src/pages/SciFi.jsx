baf328a59df4c6bb9552ac1dedaaea640d5d023c
prajina-Adhikari <aprajina45@gmail.com>
2026-09-08 22:17:07 +0545

    added new page

---
+++ src/pages/SciFi.jsx
@@ -0,0 +1,11 @@
+import GenrePage from "../components/GenrePage.jsx";
+
+export default function SciFi() {
+  return (
+    <GenrePage
+      genreId={878}
+      title="Sci-Fi"
+      description="Future worlds and speculative stories."
+    />
+  );
+}
