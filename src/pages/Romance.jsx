9a1f0d6b5033fd0af105570f0296f495993135ef
prajina-Adhikari <aprajina45@gmail.com>
2026-09-08 22:16:37 +0545

    added new page

---
+++ src/pages/Romance.jsx
@@ -0,0 +1,11 @@
+import GenrePage from "../components/GenrePage.jsx";
+
+export default function Romance() {
+  return (
+    <GenrePage
+      genreId={10749}
+      title="Romance"
+      description="Love stories, picked for a night in."
+    />
+  );
+}
