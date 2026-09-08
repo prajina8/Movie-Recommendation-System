bbaa43660ba0c0d99e1e32c7943d6906f24fe11f
prajina-Adhikari <aprajina45@gmail.com>
2026-09-08 22:12:34 +0545

    added crime page

---
+++ src/pages/Crime.jsx
@@ -0,0 +1,11 @@
+import GenrePage from "../components/GenrePage.jsx";
+
+export default function Crime() {
+  return (
+    <GenrePage
+      genreId={80}
+      title="Crime"
+      description="Heists, investigations, and the people running both sides."
+    />
+  );
+}
